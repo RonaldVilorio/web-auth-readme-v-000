75b23310a34077bfaeaeaaed90e950cb7e007de5
ronald vilorio <soccer23415@gmail.com>
2018-06-19 21:54:25 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,4 +14,11 @@ def create
   session[:token] = body["access_token"]
   redirect_to root_path
 end
+def friends
+  resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+    req.params['oauth_token'] = session[:token]
+    # don't forget that pesky v param for versioning
+    req.params['v'] = '20160201'
+  end
+  @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
 end
